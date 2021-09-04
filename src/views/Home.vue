17fb6d94a2cdce5d300ffb76d33ef98a3540d6b6
mstanka <mice78.mm@gmail.com>
2021-09-04 17:52:12 +0200

    :rocket: feat: add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,64 @@
 <template>
-  <div class="home">
-    <h1>Home page</h1>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="intro">
+    <img
+      class="intro__img"
+      src="https://images.unsplash.com/photo-1547700055-b61cacebece9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+      alt=""
+    />
+    <div class="intro__img-overlay"></div>
+
+    <div class="intro__title">
+      <h1 class="intro__title-main">Star Wars</h1>
+      <h2>Discover persons and vehicles...</h2>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
+
+<style lang="scss" scoped>
+@import "../style/main.scss";
+
+.intro {
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+
+  &__img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+
+    &-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      background: rgba(0, 0, 0, 0.5);
+    }
+  }
+
+  &__title {
+    @include flex(column, center);
+    position: relative;
+    text-transform: uppercase;
+    min-height: 100vh;
+    color: $primary-color;
+
+    &-main {
+      margin-bottom: 3rem;
+    }
+  }
+}
+</style>
